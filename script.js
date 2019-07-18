5cd013a73e6e913ef7fa0f59638e5f810f9dded6
Will Brown <williambrown@williams-air.lan>
2019-07-17 21:49:01 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var shoutouts = [
+  "You stay so positive and it's infectious!",
+  "You always the environment very positive, even if we are struggling to make our code work :)",
+  "you are always super patient when helping people and are a passionate instructor",
+  "how fun you make the lessons and how easy it is to ask you questions.",
+  "Your smile! And ability to teach things very well. Also how you bounce.",
+  "Your positivity and how fun and comfortable it is to talk to you!",
+  "your fun and positive personality! You make everything enjoyable!",
+  "how enthusiastic you are. The moment I saw your exuberant smile on the first day, I knew camp was going to be great and full of energy.",
+  "that positive you are all the time. It makes camp a funner place.",
+  "Your smile and how welcoming you are!",
+  "Is your unwavering positivity, both in solving technical problems and in everyday activities.",
+  "Your enthusiasm + willingness to help everyone!",
+  "how amazing listening to you teach is. You are such a bubbly, sweet, and kind teacher - one I wish I had in school. You are such an inspiration for me. I wish I could be as optimistic as you. Thanks for making my experience at Kode with Klossy one of the best summer camps I have ever gone to.",
+  "Your everyday happiness and excitement inspire me to keep working on my projects. Love that you say 'sweet' all the time, I think is sweet."
+]
+
+for (x of shoutouts) {
+  $("#shoutout-container").append("<p>" + x + "</p> <br>");
+}
